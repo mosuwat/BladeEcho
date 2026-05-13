14ca8fcf29a3e9e0eddd809b400dabf3c8f595a2
mosuwat <62417783+mosuwat@users.noreply.github.com>
2026-05-13 14:15:06 +0700

    ts should fix bow n arrow not loaded

--- Images/tilesets/Pixel Crawler - Free Pack/Weapons/Wood/Wood.tsx
+++ Images/tilesets/Pixel Crawler - Free Pack/Weapons/Wood/Wood.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.12.0" name="Wood" tilewidth="16" tileheight="16" tilecount="84" columns="12">
- <image source="Pixel Crawler - Free Pack/Weapons/Wood/Wood.png" width="192" height="112"/>
+ <image source="Wood.png" width="192" height="112"/>
 </tileset>
